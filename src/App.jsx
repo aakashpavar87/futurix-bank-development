854d70a3777264eb7f64adf9cbd889168f3c7fd5
Bharat Prajapati <bharat.prajapati911@outlook.com>
2024-04-06 19:49:43 +0530

    Add Privacy policy page to Footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import HowToUse from './components/HowToUse';
 import ContactUs from './components/./ContactUs';
 import TermsAndConditions from './components/TermsAndConditions';
 import Exploreus from './components/Exploreus';
+import PrivacyPolicy from './components/PrivacyPolicy';
 
 const App = () => (
- 
         // <HowToUse />
 //     <ContactUs/> 
 // <TermsAndConditions/> 
 //    <Exploreus />
+        <PrivacyPolicy />
 );
 
 export default App;
